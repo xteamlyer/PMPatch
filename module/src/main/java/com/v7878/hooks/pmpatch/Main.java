a4bf3923c931991a9084b3099b68e7a57363b684
v7878 <46866705+vova7878@users.noreply.github.com>
2024-12-09 16:16:25 +0700

    Refactor injection in system_server

--- module/src/main/java/com/v7878/hooks/pmpatch/Main.java
+++ module/src/main/java/com/v7878/hooks/pmpatch/Main.java
@@ -11,14 +11,18 @@ import com.v7878.zygisk.ZygoteLoader;
 @DoNotShrinkType
 @DoNotObfuscateType
 public class Main {
-    public static String TAG = "PM_PATCH";
+    public static String TAG = "PM_PATCH_TEST";
 
     @SuppressWarnings({"unused", "ConfusingMainMethod"})
     @DoNotShrink
     @DoNotObfuscate
     public static void main() throws Throwable {
         Log.w(TAG, "Injected into " + ZygoteLoader.getPackageName());
-        ZygoteInitHook.init();
+        try {
+            MethodAndArgsCallerHook.init();
+        } catch (Throwable th) {
+            Log.e(TAG, "Exception", th);
+        }
         Log.w(TAG, "Done");
     }
 }
---
+++ module/src/main/java/com/v7878/hooks/pmpatch/MethodAndArgsCallerHook.java
@@ -0,0 +1,47 @@
+package com.v7878.hooks.pmpatch;
+
+import static android.os.Build.VERSION.SDK_INT;
+import static com.v7878.hooks.pmpatch.Main.TAG;
+import static com.v7878.unsafe.Reflection.getDeclaredMethod;
+
+import android.annotation.SuppressLint;
+import android.util.Log;
+
+import com.v7878.r8.annotations.DoNotShrink;
+import com.v7878.unsafe.invoke.EmulatedStackFrame;
+import com.v7878.unsafe.invoke.Transformers;
+import com.v7878.vmtools.Hooks;
+import com.v7878.vmtools.Hooks.EntryPointType;
+import com.v7878.zygisk.ZygoteLoader;
+
+@SuppressLint("PrivateApi")
+public class MethodAndArgsCallerHook {
+    private static final String SYSTEM_SERVER = "com.android.server.SystemServer";
+    private static final String RUNTIME_INIT = "com.android.internal.os.RuntimeInit";
+
+    private static void checkSystemServer(EmulatedStackFrame frame) throws Throwable {
+        var accessor = frame.accessor();
+        if (SYSTEM_SERVER.equals(accessor.getReference(0))) {
+            ClassLoader loader = accessor.getReference(2);
+            ApplicationHook.init(ZygoteLoader.PACKAGE_SYSTEM_SERVER, loader);
+        }
+    }
+
+    @DoNotShrink
+    public static void init() throws Throwable {
+        Class<?> init_class = Class.forName(RUNTIME_INIT);
+
+        String method_name = SDK_INT == 26 ? "invokeStaticMain" : "findStaticMain";
+        var method = getDeclaredMethod(init_class, method_name,
+                String.class, String[].class, ClassLoader.class);
+
+        Hooks.hook(method, EntryPointType.CURRENT, (original, frame) -> {
+            try {
+                checkSystemServer(frame);
+            } catch (Throwable th) {
+                Log.e(TAG, "Exception", th);
+            }
+            Transformers.invokeExactWithFrame(original, frame);
+        }, EntryPointType.DIRECT);
+    }
+}
